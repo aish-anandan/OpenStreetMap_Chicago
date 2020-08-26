716884e15e1129e9d8d2613d6ac1d2ba50962872
aish-anandan <aish.anandan08@gmail.com>
2020-08-26 09:13:38 -0500

    Removed assertion code and added comments

--- DataParsing_ChicagoOSM.py
+++ DataParsing_ChicagoOSM.py
@@ -8,15 +8,13 @@ Created on Sat Jun 13 19:02:11 2020
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import xml.etree.cElementTree as ET
-import pprint
 import re
 import codecs
 import json
 """
-Your task is to wrangle the data and transform the shape of the data
-into the model we mentioned earlier. The output should be a list of dictionaries
-that look like this:
-
+Data in .osm file is in the form of XML. There are different kinds of attributes. In this example, we only want tags where the attribute type is node, 
+has a valid identification - name or amenity and has a lat-long position.
+Parsed data will be in the following format and will be read into a MongoDB database
 {
 "id": "2406124091",
 "type: "node",
@@ -39,61 +37,9 @@ that look like this:
 "name": "La Cabana De Don Luis",
 "phone": "1 (773)-271-5176"
 }
-
-You have to complete the function 'shape_element'.
-We have provided a function that will parse the map file, and call the function with the element
-as an argument. You should return a dictionary, containing the shaped data for that element.
-We have also provided a way to save the data in a file, so that you could use
-mongoimport later on to import the shaped data into MongoDB. 
-
-Note that in this exercise we do not use the 'update street name' procedures
-you worked on in the previous exercise. If you are using this code in your final
-project, you are strongly encouraged to use the code from previous exercise to 
-update the street names before you save them to JSON. 
-
-In particular the following things should be done:
-- you should process only 2 types of top level tags: "node" and "way"
-- all attributes of "node" and "way" should be turned into regular key/value pairs, except:
-    - attributes in the CREATED array should be added under a key "created"
-    - attributes for latitude and longitude should be added to a "pos" array,
-      for use in geospacial indexing. Make sure the values inside "pos" array are floats
-      and not strings. 
-- if the second level tag "k" value contains problematic characters, it should be ignored
-- if the second level tag "k" value starts with "addr:", it should be added to a dictionary "address"
-- if the second level tag "k" value does not start with "addr:", but contains ":", you can
-  process it in a way that you feel is best. For example, you might split it into a two-level
-  dictionary like with "addr:", or otherwise convert the ":" to create a valid key.
-- if there is a second ":" that separates the type/direction of a street,
-  the tag should be ignored, for example:
-
-<tag k="addr:housenumber" v="5158"/>
-<tag k="addr:street" v="North Lincoln Avenue"/>
-<tag k="addr:street:name" v="Lincoln"/>
-<tag k="addr:street:prefix" v="North"/>
-<tag k="addr:street:type" v="Avenue"/>
-<tag k="amenity" v="pharmacy"/>
-
-  should be turned into:
-
-{...
-"address": {
-    "housenumber": 5158,
-    "street": "North Lincoln Avenue"
-}
-"amenity": "pharmacy",
-...
-}
-
-- for "way" specifically:
-
-  <nd ref="305896090"/>
-  <nd ref="1719825889"/>
-
-should be turned into
-"node_refs": ["305896090", "1719825889"]
 """
 
-FILENAME = "./Downloads/Chicago.osm/Chicago.osm"
+FILENAME = "<path>/Chicago.osm"
 lower = re.compile(r'^([a-z]|_)*$')
 lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
 problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
@@ -140,7 +86,9 @@ def shape_element(element):
     else:
         return None
 
-
+"""
+This function dumps the parsed data into a JSON file. It also treturns a list of JSON docs that can be directly inserted into MongoDB
+"""
 def process_map(file_in, pretty = False):
     # You do not need to change this file
     file_out = "chicago_output.json"
@@ -159,34 +107,5 @@ def process_map(file_in, pretty = False):
 
     return data
 
-def test():
-    # NOTE: if you are running this code on your computer, with a larger dataset, 
-    # call the process_map procedure with pretty=False. The pretty=True option adds 
-    # additional spaces to the output, making it significantly larger.
-    data = process_map(FILENAME, False)
-    #pprint.pprint(data)
-    '''
-    correct_first_elem = {
-        "id": "261114295", 
-        "visible": "true", 
-        "type": "node", 
-        "pos": [41.9730791, -87.6866303], 
-        "created": {
-            "changeset": "11129782", 
-            "user": "bbmiller", 
-            "version": "7", 
-            "uid": "451048", 
-            "timestamp": "2012-03-28T18:31:23Z"
-        }
-    }
-    assert data[0] == correct_first_elem
-    assert data[-1]["address"] == {
-                                    "street": "West Lexington St.", 
-                                    "housenumber": "1412"
-                                      }
-    assert data[-1]["node_refs"] == [ "2199822281", "2199822390",  "2199822392", "2199822369", 
-                                    "2199822370", "2199822284", "2199822281"]
-    '''
-
 if __name__ == "__main__":
-    test()
+    data = process_map(FILENAME, False)
